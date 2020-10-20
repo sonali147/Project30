efa92f4272ff5d14c077f69c8bb9579c82ad1b3c
Sonali Gupta <sonaligupta2510@gmail.com>
2020-10-20 18:34:33 +0530

    added visibility

--- p29/Box.js
+++ p29/Box.js
@@ -8,18 +8,29 @@ class Box{
         this.height = 40;
         this.body = Bodies.rectangle(x,y,this.width,this.height, options);
         World.add(world, this.body);
+        this.visibility = 255;
         
     }
 
     display(){
-        var pos = this.body.position;
-        var angle = this.body.angle;
-        push();
-        fill("orangered");
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0, 0, this.width, this.height);
+        
+        if(this.body.speed < 3){
+            var pos = this.body.position;
+            var angle = this.body.angle;
+            push();
+            fill("orangered");
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0, 0, this.width, this.height);
+            pop();
+        } else {
+            World.remove(world, this.body);
+            this.visibility -= 5;
+            push();
+            tint(255, this.visibility);
+            pop();
+        }
         pop();
     }
 }
